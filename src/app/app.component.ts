4ef60d6bc8a2400d396619f251ed41a6180221b6
MumuzBakery <mumuzbakery@gmail.com>
2023-04-30 22:58:28 -0500

    init commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,72 @@
 import { Component } from '@angular/core';
 
+export interface Item {
+  name: string;
+  price: number;
+  quantity?: number;
+  note?: string;
+}
+
+export interface Category {
+  name: string;
+  items: Item[];
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
   title = 'mmz-bakery';
+
+  categories: Category[] = [
+    {
+      name: 'Cakes',
+      items: [
+        {name: 'Chocolate Cake', price: 10},
+        {name: 'Carrot Cake', price: 12},
+        {name: 'Red Velvet Cake', price: 15}
+      ]
+    },
+    {
+      name: 'Pastries',
+      items: [
+        {name: 'Croissant', price: 3},
+        {name: 'Danish', price: 3.5},
+        {name: 'Turnover', price: 4}
+      ]
+    }
+  ];
+
+  cartItems: Item[] = [];
+  discount = 0.85;
+
+  addItem(item: Item) {
+    let existingItem = this.cartItems.find(i => i.name === item.name);
+    if (!existingItem) {
+      this.cartItems.push({...item, quantity: 1, note: ""});
+    }
+
+    this.cartItems = this.cartItems.slice();
+  }
+
+  removeItem(item: Item) {
+    let index = this.cartItems.indexOf(item);
+    if (index > -1) {
+      this.cartItems.splice(index, 1);
+    }
+    this.cartItems = this.cartItems.slice();
+  }
+
+  getTotalPrice(): number {
+    return this.cartItems.reduce((total, item) => {
+      let quantity = 0;
+      if (item.quantity) {
+        quantity = item.quantity;
+      }
+      return parseFloat(((total + item.price * quantity) * this.discount).toFixed(2));
+    }, 0);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatListModule } from '@angular/material/list';
+import { MatIconModule } from '@angular/material/icon';
+import { MatExpansionModule } from '@angular/material/expansion'; // add this line
+import { MatTableModule } from '@angular/material/table';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -10,7 +20,16 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatSidenavModule,
+    MatListModule,
+    MatIconModule,
+    MatExpansionModule,
+    MatTableModule,    
+    MatInputModule,
+    MatFormFieldModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
